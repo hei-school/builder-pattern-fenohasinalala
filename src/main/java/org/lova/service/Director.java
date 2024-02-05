3c9ede711c0f7f0f394d4ab74d8e77da80da5461
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-02-05 18:55:01 +0300

    feat: add validator for director

--- src/main/java/org/lova/service/Director.java
+++ src/main/java/org/lova/service/Director.java
@@ -2,11 +2,17 @@ package org.lova.service;
 
 public class Director {
   public void constructSportsCar(Builder builder) {
+    if (builder==null){
+      throw new RuntimeException("Builder is mandatory");
+    }
     builder.setSeats(2);
     builder.setEngine("Eight+ Cylinder Engines");
   }
 
   public void constructSuv(Builder builder) {
+    if (builder==null){
+      throw new RuntimeException("Builder is mandatory");
+    }
     builder.setSeats(4);
     builder.setEngine("Inline Six Cylinder Engine");
   }
